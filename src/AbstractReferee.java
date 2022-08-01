3d75fdc01162f79d1649cdded9060cf9ab7f829a
ZhigangZhang <playflute@icloud.com>
2022-08-01 10:20:11 +0800

    set up project by Project Manager Austin

---
+++ src/AbstractReferee.java
@@ -0,0 +1,21 @@
+
+public abstract class AbstractReferee 
+{
+	private String name;
+
+	public AbstractReferee(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public abstract String judge(ChessBoard c);
+
+}
